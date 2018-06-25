6a556b007a17f3300da007702d2e4c53e9802c5b
Dmytro <dim.malashenko@gmai.com>
2018-06-25 12:45:07 -0700

    Added fastlane, pods

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "info.Sa2rdayMorning.TradeCoinInvest" } // The bundle identifier of your app
+var appleID: String { return "l17701240@icloud.com" } // Your Apple email address
+
+var itcTeam: String? { return "117820290" } // iTunes Connect Team ID
+var teamID: String { return "JU52RUCP3F" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
